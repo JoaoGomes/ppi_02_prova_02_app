cf32cdb78ec3b8a4a14352fdac4e62c3e0f78de5
João Gomes <joaoeduardocostagomes@hotmail.com>
2022-01-19 17:53:06 -0300

    Acréscimo de mais bibliotecas (Expo e Formik) para navegação e login

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { useState } from 'react';
+import { Image, StyleSheet, TouchableOpacity, View } from 'react-native';
+import { Input, Text } from 'react-native-elements';
 
 export default function App() {
+
+  const [email, setEmail] = useState(null);
+  const [senha, setSenha] = useState(null);
+  
   return (
     <View style={styles.container}>
       <Text style={{color: '#888', fontSize: 18}}> 
         Aplicativo da Cooperativa Santa Clara
       </Text>
+
+      <Input 
+        placeholder = "E-mail" 
+        keyboardType="email-address" 
+        onChangeText={value => setEmail(value)}/>
+
+      <Input 
+        placeholder = "Senha" 
+        secureTextEntry={true}
+        onChangeText={value => setSenha(value)}/>
 
       <TouchableOpacity
         onPress={() => alert('Em construção')}
